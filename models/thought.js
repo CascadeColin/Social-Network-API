fd46628a93b822c3bbca91df559b4084c9aed44c
Colin Marshall <colin.marshall94@gmail.com>
2023-02-09 18:30:35 -0800

    removed moment (deprecated)

--- models/thought.js
+++ models/thought.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require("mongoose");
-const moment = require("moment");
 const reactionSchema = require("./reaction");
 
 const thoughtSchema = new Schema(
@@ -13,8 +12,8 @@ const thoughtSchema = new Schema(
     createdAt: {
       type: Date,
       // creates a Date object formatted as "February 9th 2023, 2:33:56 pm"
-      default: moment().format("MMMM Do YYYY, h:mm:ss a"),
-      // NOTE: Moment lets me format the time as the Date object is being created. It would be redundant to use a getter to format it, but leaving this comment to address that since it's in the requirements.
+      default: Date.now(),
+      // TODO: getter to format the time
     },
     // user that created this thought
     username: {
