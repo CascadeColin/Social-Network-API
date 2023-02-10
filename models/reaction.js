5993ab67be71a05a82dca2be76754e369fd97da8
Colin Marshall <colin.marshall94@gmail.com>
2023-02-10 10:42:26 -0800

    added getter for formatting timestamps into local user's format

--- models/reaction.js
+++ models/reaction.js
@@ -1,5 +1,8 @@
 const { Schema, Types } = require("mongoose");
 
+// gets the timezone at the location this app is being run
+const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
 const reactionSchema = new Schema({
   reactionId: {
     type: Schema.Types.ObjectId,
@@ -17,9 +20,16 @@ const reactionSchema = new Schema({
   },
   createdAt: {
     type: Date,
-    // creates a Date object formatted as "February 9th 2023, 2:33:56 pm"
-    default: Date.now()
-    // TODO: getter to format the time
+    default: new Date(),
+    get: (epoch) => {
+      // gets info on the location this model is run from
+      const userLocation = Intl.DateTimeFormat().resolvedOptions();
+      const userLocale = userLocation.locale;
+      const userTimezone = userLocation.userTimezone;
+      // formats tiemstamp to user's language settings and timezone
+      // appears in this format: 2/10/2023, 10:22:19 AM
+      return epoch.toLocaleString(userLocale, { timeZone: userTimezone });
+    },
   },
 });
 
--- models/thought.js
+++ models/thought.js
@@ -12,8 +12,16 @@ const thoughtSchema = new Schema(
     createdAt: {
       type: Date,
       // creates a Date object formatted as "February 9th 2023, 2:33:56 pm"
-      default: Date.now(),
-      // TODO: getter to format the time
+      default: new Date(),
+      get: (epoch) => {
+        // gets info on the location this model is run from
+        const userLocation = Intl.DateTimeFormat().resolvedOptions();
+        const userLocale = userLocation.locale;
+        const userTimezone = userLocation.userTimezone;
+        // formats tiemstamp to user's language settings and timezone
+        // appears in this format: 2/10/2023, 10:22:19 AM
+        return epoch.toLocaleString(userLocale, { timeZone: userTimezone });
+      },
     },
     // user that created this thought
     username: {
@@ -32,12 +40,10 @@ const thoughtSchema = new Schema(
 );
 
 //TEST: Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
-thoughtSchema
-  .virtual("reactionCount")
-  .get(function () {
-    // returns length of the array to represent total # of reactions
-    return this.reactions.length;
-  })
+thoughtSchema.virtual("reactionCount").get(function () {
+  // returns length of the array to represent total # of reactions
+  return this.reactions.length;
+});
 
 const Thought = model("thought", thoughtSchema);
 
