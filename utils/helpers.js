abdafa2864d60a4fab5583868f3dcda98a2960f4
Colin Marshall <colin.marshall94@gmail.com>
2023-02-18 18:05:01 -0800

    added array shuffler

--- utils/helpers.js
+++ utils/helpers.js
@@ -12,5 +12,24 @@ module.exports = {
     let randomAmount = Math.floor(Math.random() * difference);
     randomAmount += min;
     return randomAmount;
+  },
+  shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+  
+    return array;
   }
 };
