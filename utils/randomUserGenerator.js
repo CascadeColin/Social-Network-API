e3342247b9aa7bd540e1c56762d50a069bf8deb9
Colin Marshall <colin.marshall94@gmail.com>
2023-02-18 18:06:22 -0800

    added helper function for quickly generating new user objects during presentations

--- utils/randomUserGenerator.js
+++ utils/randomUserGenerator.js
@@ -1,3 +1,9 @@
+// used to quickly create new users during presentation
+
 const { createRandomUser } = require("./data");
 
-console.log(createRandomUser())
+console.log(JSON.stringify(createRandomUser()));
+console.log(JSON.stringify(createRandomUser()));
+console.log(JSON.stringify(createRandomUser()));
+console.log(JSON.stringify(createRandomUser()));
+console.log(JSON.stringify(createRandomUser()));
