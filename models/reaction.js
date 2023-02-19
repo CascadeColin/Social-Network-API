2417ac515f7b4c45140ee3d8f826bbc9e9f4581d
Colin Marshall <colin.marshall94@gmail.com>
2023-02-18 18:05:28 -0800

    fixed bugs in model getters

--- models/reaction.js
+++ models/reaction.js
@@ -3,34 +3,39 @@ const { Schema, Types } = require("mongoose");
 // gets the timezone at the location this app is being run
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-const reactionSchema = new Schema({
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
-  },
-  reactionBody: {
-    type: String,
-    required: true,
-    minLength: 1,
-    maxLength: 280,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: new Date(),
-    get: (epoch) => {
-      // gets info on the location this model is run from
-      const userLocation = Intl.DateTimeFormat().resolvedOptions();
-      const userLocale = userLocation.locale;
-      const userTimezone = userLocation.userTimezone;
-      // formats tiemstamp to user's language settings and timezone
-      // appears in this format: 2/10/2023, 10:22:19 AM
-      return epoch.toLocaleString(userLocale, { timeZone: userTimezone });
+const reactionSchema = new Schema(
+  {
+    reactionBody: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: new Date,
+      get: (epoch) => {
+        // gets info on the location this model is run from
+        const userLocation = Intl.DateTimeFormat().resolvedOptions();
+        const userLocale = userLocation.locale;
+        const userTimezone = userLocation.userTimezone;
+        // formats tiemstamp to user's language settings and timezone
+        // appears in this format: 2/10/2023, 10:22:19 AM
+        return epoch.toLocaleString(userLocale, { timeZone: userTimezone });
+      },
     },
   },
-});
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
 module.exports = reactionSchema;
--- models/thought.js
+++ models/thought.js
@@ -12,7 +12,7 @@ const thoughtSchema = new Schema(
     createdAt: {
       type: Date,
       // creates a Date object formatted as "February 9th 2023, 2:33:56 pm"
-      default: new Date(),
+      default: new Date,
       get: (epoch) => {
         // gets info on the location this model is run from
         const userLocation = Intl.DateTimeFormat().resolvedOptions();
@@ -33,13 +33,14 @@ const thoughtSchema = new Schema(
   },
   {
     toJSON: {
+      getters: true,
       virtuals: true,
     },
     id: false,
   }
 );
 
-//TEST: Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+//Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
 thoughtSchema.virtual("reactionCount").get(function () {
   // returns length of the array to represent total # of reactions
   return this.reactions.length;
